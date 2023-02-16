410805d716c3cf88cb75f14f86d20bf98293cd97
unknown <baselsalah2053@gmail.com>
2023-02-16 22:55:17 +0200

    header

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  showOverlay = false;
+  @ViewChild('sideMenu') sideMenu: any;
 
+  hideMenu() {
+    this.showOverlay = false;
+    (this.sideMenu.nativeElement as HTMLElement).classList.remove(
+      'mobile-side-menu__active'
+    );
+  }
+  showMenu() {
+    this.showOverlay = true;
+    (this.sideMenu.nativeElement as HTMLElement).classList.add(
+      'mobile-side-menu__active'
+    );
+  }
 }
